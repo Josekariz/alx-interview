49959d0d8fe43547f18bed2f7a6ddc64bb338a5b
Joseph <sejokarizz@gmail.com>
2024-03-06 14:58:39 +0300

    refactor code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,32 @@
 def pascal_triangle(n):
     """
-    Returns a list of lists representing the Pascal's Triangle of n.
+    Generate Pascal's Triangle up to the nth row.
     """
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Start with the first row
+    triangle = [[1]]
 
     for i in range(1, n):
         prev_row = triangle[-1]
-        curr_row = [1]  # Each row starts and ends with 1
+        new_row = [1]  # First element is always 1
 
-        # Calculate the inner elements of the current row
-        for j in range(1, len(prev_row)):
-            curr_row.append(prev_row[j - 1] + prev_row[j])
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
 
-        curr_row.append(1)  # Add the final 1 to the current row
-        triangle.append(curr_row)
+        new_row.append(1)  # Last element is always 1
+        triangle.append(new_row)
 
     return triangle
+
+
+# Test cases
+if __name__ == "__main__":
+    def print_triangle(triangle):
+        """
+        Print the triangle
+        """
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+
+    print_triangle(pascal_triangle(5))
